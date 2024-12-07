d5515b6409062a8346264d571df415498e2dcf09
Cythonic1 <jfjbn80@gmail.com>
2024-12-07 20:23:09 +0800

    init

---
+++ hello.go
@@ -0,0 +1,15 @@
+
+package main
+
+import "fmt"
+import "sort"
+
+var myint int = 10;
+func main() {
+    // Can not be set and not used
+    myvar := 10
+    fmt.Println("Hello, world.")
+    fmt.Println(myvar);
+    i := 10
+    fmt.Println(i)
+}
