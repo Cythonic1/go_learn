d679ac1168ba7a1851b6d19b2d931ce682b61fbc
Cythonic1 <jfjbn80@gmail.com>
2024-12-09 15:58:31 +0800

    implement POST request

--- learn_by_example/RestAPI.go
+++ learn_by_example/RestAPI.go
@@ -1,9 +1,10 @@
 package learnbyexample
 
-
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 
@@ -15,20 +16,32 @@ type Projects struct {
 
 }
 
+var projects = [] Projects {
+    {ID: 1, ProjectName: "Web scanner", ProjectDesc: "Rust based web testing tool", ProjectURL: "https://github.com/Cythonic1/web_tester"},
+    {ID: 2, ProjectName: "Block chain", ProjectDesc: "A block chain from scrach using my most loved programming language", ProjectURL: "https://github.com/Cythonic1/a_blockchain_system"},
+    {ID: 3, ProjectName: "Key Logger", ProjectDesc: "A simple client server based Key logger", ProjectURL: "https://github.com/Cythonic1/KeyLogger"},
+}
 func get_projects(c *gin.Context){
-    var projects = [] Projects {
-        {ID: 1, ProjectName: "Web scanner", ProjectDesc: "Rust based web testing tool", ProjectURL: "https://github.com/Cythonic1/web_tester"},
-        {ID: 2, ProjectName: "Block chain", ProjectDesc: "A block chain from scrach using my most loved programming language", ProjectURL: "https://github.com/Cythonic1/a_blockchain_system"},
-        {ID: 3, ProjectName: "Key Logger", ProjectDesc: "A simple client server based Key logger", ProjectURL: "https://github.com/Cythonic1/KeyLogger"},
-    }
     c.IndentedJSON(http.StatusOK, projects);
+}
 
+func post_projects(c *gin.Context){
+    var new_project Projects;
+    fmt.Println(c);
+    if err := c.ShouldBindJSON(&new_project); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()});
+        return;
+    }
+
+   projects = append(projects, new_project); 
+    c.JSON(http.StatusCreated, new_project);
 
 }
 func RestApiSimple(){
 
     router := gin.Default();
     router.GET("/Yasser", get_projects);
+    router.POST("/Yasser", post_projects);
     router.Run(":8000");
 
 }
